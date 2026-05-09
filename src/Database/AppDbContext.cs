755c2d7e7bec4b7848026f4239586b7a6b89a88b
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-05-09 10:16:19 +0300

    Add: Configurations for entities; Update: AppDbContext

--- src/Database/AppDbContext.cs
+++ src/Database/AppDbContext.cs
@@ -9,4 +9,10 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
     public DbSet<Book> Books => Set<Book>();
     public DbSet<Author> Authors => Set<Author>();
     public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Це автоматично знайде всі класи, що реалізують IEntityTypeConfiguration
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
 }
