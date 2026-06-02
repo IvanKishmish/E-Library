c3e0393c6f1f8122b3a2bc6994694bc0314304b7
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-06-02 23:21:34 +0300

    refactor(domain): update core entities and add global usings

--- src/ELibrary.Domain/Entities/Author.cs
+++ src/ELibrary.Domain/Entities/Author.cs
@@ -1,6 +1,46 @@
 namespace ELibrary.Domain.Entities;
 
-public class Author
+using ErrorOr;
+
+public class Author : Entity<EntityId>
 {
+    public string FirstName { get; private set;} = string.Empty;
+    public string LastName { get; private set;} = string.Empty;
+    public string Biography { get; private set;} = string.Empty;
+    public ICollection<Book> Books { get; private set;} = new List<Book>();
     
+    private Author(){}//ef
+
+    private Author(EntityId id, string firstName, string lastName, string biography)
+    : base(id) //з protected Entity(TId id) => Id = id; тягнемо
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Biography = biography;
+        // Список книг ініціалізується автоматично
+    }
+
+    public static ErrorOr<Author> Create(string firstName, string lastName, string biography)
+    {
+        if (string.IsNullOrWhiteSpace(firstName)) 
+            return Error.Validation("Author.FirstNameRequired", "First name is required.");
+            
+        if (string.IsNullOrWhiteSpace(lastName)) 
+            return Error.Validation("Author.LastNameRequired", "Last name is required.");
+        
+        // Передаємо тільки дані автора, бо книг при створенні у нього немає
+        return new Author(EntityId.CreateVersion7(), firstName, lastName, biography);
+    }
+
+    public ErrorOr<Updated> UpdateInfo(string firstName, string lastName, string biography)
+    {
+        if (string.IsNullOrWhiteSpace(firstName)) return Error.Validation("Author.FirstNameRequired");
+        if (string.IsNullOrWhiteSpace(lastName)) return Error.Validation("Author.LastNameRequired");
+        
+        FirstName = firstName;
+        LastName = lastName;
+        Biography = biography;
+
+        return Result.Updated;
+    }
 }
--- src/ELibrary.Domain/Entities/Book.cs
+++ src/ELibrary.Domain/Entities/Book.cs
@@ -1,6 +1,53 @@
 namespace ELibrary.Domain.Entities;
 
-public class Book
+using ErrorOr;
+
+public class Book : Entity<EntityId>
 {
+    public string Title { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
     
+    public EntityId CategoryId { get; private set; }
+    public EntityId AuthorId { get; private set; }
+
+    
+    public Category Category { get; private set; } = null!;
+    public Author Author { get; private set; } = null!;
+
+    private Book() {}//ef
+
+    private Book(EntityId id, string title, string description, decimal price, EntityId categoryId, EntityId authorId )
+    : base(id)
+    {
+        Title = title;
+        Description = description;
+        Price = price;
+        CategoryId = categoryId;
+        AuthorId = authorId;
+    }
+
+    public static ErrorOr<Book> Create(string title, string description, decimal price, EntityId categoryId, EntityId authorId )
+    {
+        if (string.IsNullOrWhiteSpace(title)) 
+            return Error.Validation("Book.TitleRequired", "Title cannot be empty.");
+            
+        if (price < 0) 
+            return Error.Validation("Book.InvalidPrice", "Price cannot be less than zero.");
+        
+        return new Book(EntityId.CreateVersion7(), title, description, price, categoryId, authorId);
+    }
+    
+    public ErrorOr<Updated> UpdateInfo(string title, string description, decimal price, EntityId categoryId)
+    {
+        if (string.IsNullOrWhiteSpace(title)) return Error.Validation("Book.TitleRequired");
+        if (price < 0) return Error.Validation("Book.InvalidPrice");
+        
+        Title = title;
+        Description = description;
+        Price = price;
+        CategoryId = categoryId;
+
+        return Result.Updated;
+    }
 }
