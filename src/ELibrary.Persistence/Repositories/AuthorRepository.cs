e22432951dbaaab02fa6584da29bd962e392e083
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-06-04 17:34:52 +0300

    feat(persistence): implement Category/Author/BookRepository.cs

---
+++ src/ELibrary.Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,25 @@
+using ELibrary.Application.Common.Interfaces;
+using ELibrary.Domain.Entities;
+using ELibrary.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELibrary.Persistence.Repositories;
+
+public sealed class AuthorRepository(AppDbContext context) : IAuthorRepository
+{
+    public async Task<Author?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Authors.FirstOrDefaultAsync(a => a.Id == id, cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<Author>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context.Authors
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public void Add(Author author) => context.Authors.Add(author);
+
+    public void Remove(Author author) => context.Authors.Remove(author);
+}
---
+++ src/ELibrary.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+using ELibrary.Application.Common.Interfaces;
+using ELibrary.Domain.Entities;
+using ELibrary.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELibrary.Persistence.Repositories;
+
+public sealed class CategoryRepository(AppDbContext context) : ICategoryRepository
+{
+    public async Task<Category?> GetCategoryByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        //as we use this method in commands and in queries,
+        //it's safer to remove the .AsNoTracking() because when either deleting or updating ef core has to track changes
+        return await context.Categories.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<Category>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context.Categories
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public void Add(Category category) => context.Categories.Add(category);
+
+    public void Remove(Category category) => context.Categories.Remove(category);
+}
