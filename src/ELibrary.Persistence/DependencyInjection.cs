e5c5e400776e66bdfc47ca5bfd1db2a9691f97ce
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-06-04 18:57:45 +0300

    update: add scoped registration of repositories

--- src/ELibrary.Persistence/DependencyInjection.cs
+++ src/ELibrary.Persistence/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using ELibrary.Application.Common.Interfaces;
 using ELibrary.Persistence.Context;
+using ELibrary.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,12 @@ public static class DependencyInjection
       // Налаштовуємо підключення до PostgreSQL
       services.AddDbContext<AppDbContext>(options =>
          options.UseNpgsql(configuration.GetConnectionString("DB_CONNECTION_STRING")));
+
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+      services.AddScoped<ICategoryRepository, CategoryRepository>();
+      services.AddScoped<IAuthorRepository,  AuthorRepository>();
+      services.AddScoped<IBookRepository, BookRepository>();
       
       return services;
    } 
